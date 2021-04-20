ef9fb01b81351e80d9baf4013f8c117ce2453a46
Stefano <piero.quiroz93@gmail.com>
2021-04-20 18:50:54 -0500

    Prop it Up add component class finished

--- prop-it-up/src/App.js
+++ prop-it-up/src/App.js
@@ -1,10 +1,13 @@
-
+import PersonCard from './components/PersonCard'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <PersonCard  firstName={"Joe"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+      <PersonCard  firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCard  firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCard  firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
     </div>
   );
 }
