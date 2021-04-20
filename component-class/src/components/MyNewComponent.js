4401f85436c0abb45067ba7569c550e938169a8d
Stefano <piero.quiroz93@gmail.com>
2021-04-20 17:20:56 -0500

    Component Class

---
+++ component-class/src/components/MyNewComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyNewComponent extends Component {
+    render(){
+        return (
+            <div>
+                This is our first class component from components folder.
+            </div>
+        );
+    }
+}
+
+export default MyNewComponent;
