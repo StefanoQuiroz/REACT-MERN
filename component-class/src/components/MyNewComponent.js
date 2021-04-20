42745ac6850c325f4c5a4f9fffecd853ac942b95
Stefano <piero.quiroz93@gmail.com>
2021-04-20 18:24:15 -0500

    Add this.props & destructuring { , } = this.props

--- component-class/src/components/MyNewComponent.js
+++ component-class/src/components/MyNewComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-
+//Component -> App.js
 class MyNewComponent extends Component {
     render(){
+        //destructurar para oder usar como variables firstName and LastName
+        const { firstName, lastName } = this.props;
         return (
             <div>
-                This is our first class component from components folder.
+                {/* This is our first class component from components folder. */}
+                {/* <h1>My name is {this.props.firstName} <- ACCESORIOS ->{this.props.lastName}</h1> */}
+                <h1>My name is {firstName} {lastName}</h1>
             </div>
         );
     }
