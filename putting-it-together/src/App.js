621898ad5371af675f01853af7302aa2ed089bae
Stefano <piero.quiroz93@gmail.com>
2021-04-22 00:24:35 -0500

    App.js modified

--- putting-it-together/src/App.js
+++ putting-it-together/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import PuttingItTogether from './components/PuttingItTogether'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PuttingItTogether lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"}/>
+      <PuttingItTogether lastName={"Smith"} firstName={"Jhon"} age={88} hairColor={"Brown"}/>
     </div>
   );
 }
