1d3926928efd1fc0b8c2f65ffcb73de8b376be00
Stefano <piero.quiroz93@gmail.com>
2021-04-22 09:42:12 -0500

    2 secondary components PuttingItTogether add to App.js

--- putting-it-together/src/App.js
+++ putting-it-together/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
-import PuttingItTogether from './components/PuttingItTogether'
-
+//import PersonCard from './components/PersonCards'
+//import LightSwitch from './components/LightSwitch'
+import PuttingTogether from './components/Putting-it-together'
 function App() {
   return (
     <div className="App">
-      <PuttingItTogether lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"}/>
-      <PuttingItTogether lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"}/>
+      {/* <PersonCard lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"}>
+        <p>This a child</p>
+        <p>This a another Child</p>
+        <p>This even another child</p>
+      </PersonCard> */}
+      {/* <PersonCard lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"}/> */}
+      {/* <LightSwitch /> */}
+      <PuttingTogether lastName={"Doe"} firstName={"Jane"} age={45} hairColor={"Black"} />
+      <PuttingTogether lastName={"Smith"} firstName={"John"} age={88} hairColor={"Brown"} />
     </div>
   );
 }
