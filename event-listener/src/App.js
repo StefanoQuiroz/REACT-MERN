773ef9af0a8e40484b1605798cd37f165a59bca2
Stefano <piero.quiroz93@gmail.com>
2021-04-21 20:40:00 -0500

    Apps modified

--- event-listener/src/App.js
+++ event-listener/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import LightSwitch from './components/LightSwitch'
+//import LightSwitch from './components/LightSwitch'
+import LightSwitch2 from './components/LightSwitch2'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       {/* onClick = { props tambien pude recibir una funcion ()=> console.log/alert()}, luego el boton react con el mensaje de alert("blah blah") */}
-      <button onClick = { () => alert(`Haz hecho click en este boton`) }>Haz click</button>
-      <LightSwitch />
+      {/* <button onClick = { () => alert(`Haz hecho click en este boton`) }>Haz click</button>
+      <LightSwitch /> */}
+      <LightSwitch2 />
     </div>
     
   );
