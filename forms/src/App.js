54296429822d62922acfb0aede1141fd6c05c9e6
Stefano <piero.quiroz93@gmail.com>
2021-04-30 18:55:54 -0500

    MoreForms with Hooks 0%

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,5 +1,5 @@
-//import React, {useState} from 'react'; 
-import React from 'react';
+import React, {useState} from 'react'; 
+//import React from 'react';
 //import UserFormOwn from './components/formulario-clase/UseFormOwn';
 //import NameForm from './components/formReact/NameForm';
 //import Reservation from './components/formReact/Reservation';
@@ -9,16 +9,18 @@ import React from 'react';
 //import FormRenderer from './components/renderizadoCondicional/FormRenderer'
 //import FormRendererConditional from './components/renderizadoCondicional/FormRendererConditional'
 //import ManejoDeValidaciones from './components/renderizadoCondicional/ManejoDeValidaciones'
-import FormValidation from './components/renderizadoCondicional/FormValidation'
+//import FormValidation from './components/renderizadoCondicional/FormValidation'
+import MoreForms from './components/moreForms/MoreForms'
+import './sass/sass-class/hooksForm.scss'
 const App = () => {
   //variables de configuracion
-  /* const [state, setState] = useState({
+  const [state, setState] = useState({
     firstName: "",
     lastName: "",
     email: "",
     pass: "",
     confirmPass: ""
-  }); *///se pasa a los hijos
+  });//se pasa a los hijos
   /* const [state, setState] = useState({
     userName: "",
     emailAddress: "",
@@ -36,7 +38,8 @@ const App = () => {
       {/* <FormRenderer inputs={state} setInputs={setState} /> */}
       {/* <FormRendererConditional/> */}
       {/* <ManejoDeValidaciones/> */}
-      <FormValidation/>
+      {/* <FormValidation/> */}
+      <MoreForms inputs={state} setInputs={setState} />
     </div>
   );
 }
