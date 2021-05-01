ffe68c9e013f2f4da170979044d9c4dfc31cef15
Stefano <piero.quiroz93@gmail.com>
2021-04-30 20:50:06 -0500

    More Froms without state and setState from app.js

--- forms/src/components/moreForms/MoreForms.js
+++ forms/src/components/moreForms/MoreForms.js
@@ -1,40 +1,73 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const MoreForms = (props) => {
-    const {inputs, setInputs} = props;
-    
-    const onChangeClick = (event) => {
-       const name = event.target.name;
-       const value = event.target.value;
-       setInputs({
-           ...inputs,
-           [name]:value
-       })
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+    const [confirmPass, setConfirmPass] = useState("");
+    const [error, setError] = useState("");
+
+    const onChangeClick = (e) => {
+        e.preventDefault();
+        setFirstName(e.target.value);
+        setLastName(e.target.value);
+        setEmail(e.target.value);
+        setPass(e.target.value);
+        setConfirmPass(e.target.value);
+        if(firstName.length > 0 && firstName.length < 2){
+           return setError("First name must be at least 2 characters");
+        } else if (lastName.length > 0 && lastName.length < 2){
+        return setError("First name must be at least 2 characters");
+        } else if (email.length > 0 && email.length < 5){
+            return setError("First name must be at least 5 characters");
+        } else if (pass.length === confirmPass.length){
+            return setError("Pass must match")
+        }
+       //setFirstName("");
+
    }
+
    return (
-       <form>
-           <h2>Form Register</h2>
-           <div className="row">
-               <label htmlFor="firstName">First Name: </label>
-               <input type="text" name="firstName" onChange={onChangeClick}/>
-           </div>
-           <div className="row">
-               <label htmlFor="lastName">Last Name: </label>
-               <input type="text" name="lastName" onChange={onChangeClick}/>
-           </div>
-           <div className="row">
-               <label htmlFor="email">Email: </label>
-               <input type="email" name="email" onChange={onChangeClick}/>
-           </div>
-           <div className="row">
-               <label htmlFor="pass">Password: </label>
-               <input type="password" name="pass" onChange={onChangeClick}/>
-           </div>
-           <div className="row">
-               <label htmlFor="confirmPass">Confirm Password: </label>
-               <input type="password" name="confirmPass" onChange={onChangeClick}/>
-           </div>
-       </form>
+        <form>
+            <h2>Form Register</h2>
+            <div className="row">
+                <label htmlFor="firstName">First Name: </label>
+                <input type="text" name="firstName" onChange={onChangeClick} />
+                {
+                    error ?
+                    <p>{error}</p> : ''
+                }
+            </div>
+            <div className="row">
+                <label htmlFor="lastName">Last Name: </label>
+                <input type="text" name="lastName"/>
+                {
+                    error ?
+                    <p>{error}</p> : ''
+                }
+            </div>
+            <div className="row">
+                <label htmlFor="email">Email: </label>
+                <input type="email" name="email" />
+                {
+                    error ?
+                    <p>{error}</p> : ''
+                }
+            </div>
+            <div className="row">
+                <label htmlFor="pass">Password: </label>
+                <input type="password" name="pass" />
+            </div>
+            <div className="row">
+                <label htmlFor="confirmPass">Confirm Password: </label>
+                <input type="password" name="confirmPass" />
+                {
+                    error ?
+                    <p>{error}</p> : ''
+                }
+            </div>
+        </form>
    )
 }
 
