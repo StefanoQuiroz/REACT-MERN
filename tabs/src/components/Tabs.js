64cfca790f3002052b40cf9aaef523f14f11eaa1
Stefano <piero.quiroz93@gmail.com>
2021-05-07 12:46:17 -0500

    Tabs Own Version at 70%

--- tabs/src/components/Tabs.js
+++ tabs/src/components/Tabs.js
@@ -1,32 +1,34 @@
 import React, {useState} from 'react';
 
 const Tabs = (props) => {
-    let array = [1,2,3];
+    const array = [1,2,3];
+    
     const [state, setState] = useState({
+        //array[0] inicar el currentTab: 1;
         currentTab: 1
     })
 
+    //Method onClick
     const onClickHandle = (e, value) => {
         setState({
             currentTab: value
         })
     }
-
+    //{ map() => {return button}}  === { map() => (button)}
     return (
         <div>
-            {/* map(item, index) */}
-            {array.map((item, index) => (
-                <button
-                    key={index}
-                    onClick={(e) => onClickHandle(e,item)}
-                >   
-                    Tab {item}
+            {/* Comentario: agragar styled-components */}
+            {array.map((item, index)=> (
+                <button 
+                key={index}
+                onClick={(e)=> onClickHandle(e, item)}
+                >Tab {item}
                 </button>
             ))}
-            {/* Resultado en un div cuando haga click en la pestaña */}
+            {/* Mostrar el resultado - comentario: cambiar por un p tag*/}
             <div className="results">
-                Tab {state.currentTab} content is showing here.
-            </div>            
+                Tab {state.currentTab} content is showing here
+            </div>
         </div>
     );
 }
