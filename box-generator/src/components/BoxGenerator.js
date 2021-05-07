110e225c0e2ab5d6b1a00753ee124f78bffd0e5f
Stefano <piero.quiroz93@gmail.com>
2021-05-06 21:35:30 -0500

    Box Generator Own Version at 55%

--- box-generator/src/components/BoxGenerator.js
+++ box-generator/src/components/BoxGenerator.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import StyledBox from './StyledBox'
 const BoxGenerator = () => {
+    const colorCampo = useRef();
+    const alturaCampo = useRef();
+    const anchoCampo = useRef();
+    const [state, setState] = useState({
+        boxes: [],
+        newColor: "",
+        newWidth: 0
+    })
+
+    //Methods
+    const onChange = (e) => {
+        e.preventDefault();
+        const value =e.target.value;
+        const name = e.target.value;
+        //[name]: value
+        setState({
+            ...state,
+            [name] : value
+
+        })
+    }    
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setState({
+            boxes: [...state.boxes, {color: state.newColor, width: state.newWidth}],
+            newColor: "",
+            newWidth: 0
+        })
+        colorCampo.current.value= "";
+    }
+
+
+
     return (
         <div>
-            <form className="formulario">
+            <form className="formulario" onSubmit={onSubmit}>
                 <label htmlFor="color">Color</label>
-                <input type="text" />
+                <input type="text" ref={colorCampo} onChange={onChange} name="color"/>
+                <label htmlFor="height">Height</label>
+                <input type="text" ref={alturaCampo} onChange={onChange} name="height"/>
+                <label htmlFor="width">Height</label>
+                <input type="text" ref={anchoCampo} onChange={onChange} name="width"/>
                 <input type="submit" value="Add"/>
             </form>
 
