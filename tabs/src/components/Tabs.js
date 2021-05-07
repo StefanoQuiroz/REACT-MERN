062eb2d6ec6aa866b3192c62ccff60f828c5a014
Stefano <piero.quiroz93@gmail.com>
2021-05-07 10:55:14 -0500

    Tabs Own Version at 45%

--- tabs/src/components/Tabs.js
+++ tabs/src/components/Tabs.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Tabs = () => {
+    let array = [1,2,3];
+    const [state, setState] = useState({
+        currentTab: 1
+    })
+
+    const onClickHandle = (e, value) => {
+        setState({
+            currentTab: value
+        })
+    }
+
+
+
+
     return (
         <div>
-            
+            {/* map(item, index) */}
+            {array.map((item, index) => (
+                <button
+                    key={index}
+                    onClick={(e) => onClickHandle(e,item)}
+                >   
+                    Tab {state.currentTab}
+                </button>
+            ))}
+            {/* Resultado en un div cuando haga click en la pestaña */}
+            <div className="results">
+                Tab {state.currentTab} content is showing here.
+            </div>            
         </div>
     );
 }
