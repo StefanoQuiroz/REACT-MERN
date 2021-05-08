7d6b6c548539988317087e695dc110a49bd71d1e
Stefano <piero.quiroz93@gmail.com>
2021-05-08 01:12:02 -0500

    Wrapper Own Version 100%

--- navbar-context/src/components/Wrapper.js
+++ navbar-context/src/components/Wrapper.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import MyContext from './MyContext';
+import Form from './Form'
 
-const Wrapper = () => {
+const Wrapper = (props) => {
+    const [state,setState] = useState("");
+    //<MyContext.Provider no provides /> <= Recordar
     return (
-        <div>
-            
-        </div>
+        <MyContext.Provider value={{state, setState}}>
+            <Form>
+            </Form>
+        </MyContext.Provider>
     );
 }
 
